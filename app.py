03102934949ccfb85901420fecc67fa814f73354
Bhargav <bhargav1438@outlook.com>
2025-08-06 15:43:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def subscribe():
     return jsonify({"message": "Subscription successful"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
